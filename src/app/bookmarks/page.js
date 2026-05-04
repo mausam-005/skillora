0decc7d6957762356b9275486adb536789135dbe
Mausam <mausamdwivedi2020@gmail.com>
2026-05-04 16:45:00 +0530

    restructure project into frontend and backend folders

--- src/app/bookmarks/page.js
+++
@@ -1,8 +0,0 @@
-export default function Bookmarks() {
-    return (
-      <div>
-        <h1>Bookmarks Page</h1>
-        <p>This is the bookmarks page.</p>
-      </div>
-    )
-  }
--- src/app/courses/page.js
+++
@@ -1,8 +0,0 @@
-export default function Courses() {
-    return (
-      <div>
-        <h1>Courses Page</h1>
-        <p>This is the courses page</p>
-      </div>
-    )
-  }
